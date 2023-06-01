0c597fb04eed173fd40258d2c68edab608cc3245
Paulo Henrique <paulo13579691@gmail.com>
2023-06-01 01:10:06 -0300

    starting tests

--- sutton/cap4/jacks/new/prob.py
+++ sutton/cap4/jacks/new/prob.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.stats import poisson
+import matplotlib
 import matplotlib.pyplot as plt
+matplotlib.use('module://matplotlib-backend-kitty')
 
 THRESHOLD = 0.1
 PROB_THRESHOLD = 0.0001
@@ -150,17 +152,138 @@ def calc_p(P1,P2):
         for yf in range(n):
             for x in range(n):
                 for y in range(n):
-                    i = 0
                     for a in range(-MAX_MOVES,MAX_MOVES+1):
+                        i = MAX_MOVES + a
                         if(a < -y or a > x):
                             P[xf][yf][x][y][i] = 0
-                            print(f"P[{xf}][{yf}][{x}][{y}][{a}] = {P[xf][yf][x][y][i]}")
-                            i += 1
+                            print(f"P(({xf},{yf})|({x},{y}),{a}) = {P[xf][yf][x][y][i]}")
                             continue
-                        P[xf][yf][x][y][i] = P1[xf][x][a]*P2[yf][y][a]
-                        print(f"P[{xf}][{yf}][{x}][{y}][{a}] = {P[xf][yf][x][y][i]}")
-                        i += 1
+                        P[xf][yf][x][y][i] = P1[xf][x][i]*P2[yf][y][i]
+                        print(f"P(({xf},{yf})|({x},{y}),{a}) = {P[xf][yf][x][y][i]}")
     return P
+
+def calc_r():
+    """
+    calculate r(s,a) matrix
+    """
+    n = MAX_CARS + 1
+    R = np.zeros((n,n,2*MAX_MOVES+1))
+
+    for x in range(n):
+        for y in range(n):
+            for a in range(-MAX_MOVES,MAX_MOVES+1):
+                i = MAX_MOVES + 1
+                if(y+a > MAX_CARS or x-a > MAX_CARS):
+                    R[x][y][i] = 0
+                    print(f"r(({x},{y}),{a}) = {R[x][y][i]}")
+                    continue
+                if(a < -y or a > x):
+                    R[x][y][i] = 0
+                    print(f"r(({x},{y}),{a}) = {R[x][y][i]}")
+                    continue
+                R[x][y][i] = calc_r_(x,y,a)
+                print(f"r(({x},{y}),{a}) = {R[x][y][i]}")
+
+    return R
+
+def calc_r_(x,y,a):
+    sum = 0
+    for r in range(-2*abs(a),-2*abs(a)+10*(x+y),10):
+        for p1 in range(x-a+1):
+            num_request = (r+2*abs(a))//10
+            sum += r * P1_(p1,x,a) * P2_(num_request - p1, y,a)
+    return sum
+
+def policy_evaluation(V,pi,R,P):
+    n = MAX_CARS + 1 
+
+    print("evaluating policy...")
+    while(True):
+        delta = 0
+        total = 0
+        for x in range(n):
+            for y in range(n):
+                v = V[x][y]
+                a = int(pi[x][y])
+                i = MAX_MOVES + a
+
+                #updates V(s)
+                sum = 0
+                for xf in range(n):
+                    for yf in range(n):
+                        sum += V[xf][yf]*P[xf][yf][x][y][i]
+
+                print(f"r(({x},{y}),{a}) = {R[x][y][i]}")
+                print(f"sum = {sum}")
+                V[x][y] = R[x][y][i] + sum * GAMMA
+                delta = max(delta,abs(v-V[x][y]))
+                total += 1
+                #print(f"{total}/{n*n}")
+    
+        if(delta < THRESHOLD):
+            break
+        else:
+            print(f"not done yet: delta = {delta}")
+
+    return
+
+def policy_improvement(V,pi,policy_stable,R,P):
+    print("improving policy...")
+
+    n = MAX_CARS + 1 
+    policy_stable = True
+    total = 0
+
+    for x in range(n):
+        for y in range(n):
+            a_past = pi[x][y]
+            max_arg = a_past
+            max_sum = -INFTY
+            for a in range(-MAX_MOVES,MAX_MOVES+1):
+                i = MAX_MOVES + 1
+                sum = 0
+                for xf in range(n):
+                    for yf in range(n):
+                        sum += V[xf][yf]*P[xf][yf][x][y][i]
+                sum = R[x][y][i] + sum * GAMMA
+                if(sum > max_sum):
+                    max_arg = a
+                    max_sum = sum
+
+            pi[x][y] = max_arg
+            if(a_past != pi[x][y]):
+                policy_stable = False
+            total += 1
+            if(total == (n*n)//4): print("25%")
+            if(total == (n*n)//2): print("50%")
+            if(4*total == 3*(n*n)): print("75%")
+            if(total == (n*n)): print("100%")
+            #print(f"{total}/{n*n}")
+    return
+
+def show(pi):
+
+    n = len(pi)
+
+    cmap = plt.cm.get_cmap('RdYlBu',11)
+    norm = plt.Normalize(-5, 5)  # Normalizar os valores de pi(x, y) entre -5 e 5
+
+    plt.imshow(pi,cmap = cmap, extent = [0,n-1,0,n-1])
+
+    cbar = plt.colorbar()
+    cbar.set_label('pi(x, y)')
+
+    # Configurar os rótulos dos eixos
+    plt.xticks(np.arange(n))
+    plt.yticks(np.arange(n))
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    # Configurar os limites dos eixos
+    plt.xlim(0, n-1)
+    plt.ylim(0, n-1)
+
+    plt.show()
 
 def main():
 
@@ -170,8 +293,8 @@ def main():
     P2 = calc_p2()
 
     
-    print()
-    print()
+    #print()
+    #print()
 
     #x = 19 
     #a = -3 
@@ -212,7 +335,76 @@ def main():
 
     #testing P
 
+    #debuging p(20,20|20,19,5) should be zero because y+a > MAX_CARS
+    #xf = 20; yf = 20
+    #x = 20; y = 19
+    #a = -1
+    #i = MAX_MOVES + a
+
+    #print()
+    #print()
+    #print(f"p({xf},{yf}|{x},{y},{a}) = {P[xf][yf][x][y][i]}")
+
+    #print(f"p(x' = {xf}|x = {x},a = {a}) = {P1[xf][x][i]}")
+
+    #print(f"p(y' = {yf}|y = {y},a = {a}) = {P2[yf][y][i]}")
+    #return
+
+
+    #checking sums
+
+    #for x in range(n):
+    #    for y in range(n):
+    #        for a in range(-MAX_MOVES,MAX_MOVES+1):
+    #            i = MAX_MOVES + a
+    #            sum = 0
+    #            for xf in range(n):
+    #                for yf in range(n):
+    #                    sum += P[xf][yf][x][y][i]
+    #            print(f"sum(p(s'|({xf},{yf}),{a}))={sum}")
+
+    R = calc_r()
+
+
+
+    V = np.zeros((n,n)) #inicialize value function with zeroes
+    pi = np.zeros((n,n)) #inicialize policy with a = 0 for all s
+
+    print()
+    print(f"pi[0]:\n {pi}")
+    #show(pi)
+
+    print()
+    print(f"V[0]:\n {V}")
+
+    policy_stable = False
+    i = 1
+    while(not(policy_stable)):
+        policy_evaluation(V,pi,R,P)
+        print()
+        print("value function evaluated!")
+        print()
+        print(f"V[{i}]:\n {V}")
+        policy_improvement(V,pi,policy_stable,R,P)
+        i += 1
+        if(not(policy_stable)):
+            print()
+            print("policy improved!")
+            print()
+            print(f"pi[{i}]:\n {pi}")
+            #show(pi)
+
+
+    print()
+    print("final policy")
+    print(f"pi[{i}]:\n {pi}")
+    #show(pi)
+    print()
+    print("final value function")
+    print(f"V[{i}]:\n {V}")
+
     return
+
 
 if __name__ == "__main__":
     main()
